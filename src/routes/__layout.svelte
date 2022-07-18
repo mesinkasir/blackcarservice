c48632a60b808e0409aca694a361898606d7885e
chromers <digitalartcreative@hotmail.com>
2022-07-19 01:08:28 +0700

    new update fix

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,6 @@
 <script>
 	import Nav from '$lib/widget/Navi.svelte';
-	import Menu from '$lib/widget/Menu.svelte';
+	import Button from '$lib/widget/Button.svelte';
 	import Cover from '$lib/widget/Cover.svelte';
 	import Header from '$lib/widget/Header.svelte';
 	import Footer from '$lib/widget/Footer.svelte';
@@ -13,7 +13,7 @@
 		<div class="col-md-7 p-3 p-md-5 opacity-75">
 			<Header/>
 		<hr/>
-		<Menu/>
+		<Button/>
 		<hr/>
 		<slot />
 	</div>
