da3cf260f871974f904ee9c029c8a5a36687aa4f
chromers <digitalartcreative@hotmail.com>
2022-07-19 01:15:31 +0700

    new update config

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,18 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+export default {
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false
+		}),
 
-		// Override http methods in the Todo forms
-		methodOverride: {
-			allowed: ['PATCH', 'DELETE']
+		prerender: {
+			// This can be false if you're using a fallback (i.e. SPA mode)
+			default: true
 		}
 	}
 };
-
-export default config;
